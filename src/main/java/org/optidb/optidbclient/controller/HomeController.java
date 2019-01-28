5a1386bb987c6afcc72cdf5b66917600a1110ae8
Haseeb Chaudhry <haseeb.chaudhry@hotmail.fr>
2019-01-28 01:00:39 +0100

    Adding template 'Brilliant' and dependencies in pom.xml

--- src/main/java/org/optidb/optidbclient/controller/HomeController.java
+++ src/main/java/org/optidb/optidbclient/controller/HomeController.java
@@ -1,22 +1,26 @@
 package org.optidb.optidbclient.controller;
 
+import javafx.application.Application;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
 
 @Controller
-public class HomeController {
+public class HomeController
+{
 
-    @Value("${spring.application.name}")
-    String appName;
-
-    @GetMapping("/home")
-    public String homePage(Model model) {
-        model.addAttribute("appName", appName);
+    @GetMapping({"/","/home"})
+    public String home()
+    {
         return "home";
     }
+
 }
+
