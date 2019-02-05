6c8fc631c5523080fcf4ae97613fbb6c261b8c17
Haseeb Chaudhry <haseeb.chaudhry@hotmail.fr>
2019-02-05 12:55:21 +0100

    Remove unused code

--- src/test/java/org/optidb/optidbclient/OptidbClientApplicationTests.java
+++ src/test/java/org/optidb/optidbclient/OptidbClientApplicationTests.java
@@ -7,11 +7,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class OptidbClientApplicationTests {
-
+public class OptidbClientApplicationTests
+{
 	@Test
-	public void contextLoads() {
-	}
+	public void contextLoads()
+	{
 
+	}
 }
 
